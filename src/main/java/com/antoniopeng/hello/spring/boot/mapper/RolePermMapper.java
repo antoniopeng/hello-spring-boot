5cbfcaf18cf7848920502a65ec9ee7d5fc01f8db
pc1688 <1623763095@qq.com>
2019-08-20 16:42:51 +0800

    update

--- src/main/java/com/antoniopeng/hello/spring/boot/mapper/RolePermMapper.java
+++
@@ -1,7 +0,0 @@
-package com.antoniopeng.hello.spring.boot.mapper;
-
-import com.antoniopeng.hello.spring.boot.common.MyMapper;
-import com.antoniopeng.hello.spring.boot.entity.RolePerm;
-
-public interface RolePermMapper extends MyMapper<RolePerm> {
-}
--- src/main/java/com/antoniopeng/hello/spring/boot/mapper/UserRoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.antoniopeng.hello.spring.boot.mapper;
-
-import com.antoniopeng.hello.spring.boot.common.MyMapper;
-import com.antoniopeng.hello.spring.boot.entity.UserRole;
-
-public interface UserRoleMapper extends MyMapper<UserRole> {
-}
